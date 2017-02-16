7b3d08aa8f90321ed1cf7c35952e33444422c296
Florian Walbroel <florian.walbroel@rwth-aachen.de>
2017-02-16 13:07:02 +0100

    fixed old/wrong argument for isotropic distribution

--- aps/evaluate.sh
+++ aps/evaluate.sh
@@ -3,11 +3,11 @@
 mkdir -p results
 
 # isotropic
-python aps.py -n 90 -l 10 -i -c 100 -s 600 > results/iso_10.txt & # 10%
-python aps.py -n 80 -l 20 -i -c 100 -s 600 > results/iso_20.txt & # 20%
-python aps.py -n 70 -l 30 -i -c 100 -s 600 > results/iso_30.txt & # 30%
-python aps.py -n 60 -l 40 -i -c 100 -s 600 > results/iso_40.txt & # 40%
-python aps.py -n 50 -l 50 -i -c 100 -s 600 > results/iso_50.txt & # 50%
+python aps.py -n 90 -l 10 --iso -c 100 -s 600 > results/iso_10.txt & # 10%
+python aps.py -n 80 -l 20 --iso -c 100 -s 600 > results/iso_20.txt & # 20%
+python aps.py -n 70 -l 30 --iso -c 100 -s 600 > results/iso_30.txt & # 30%
+python aps.py -n 60 -l 40 --iso -c 100 -s 600 > results/iso_40.txt & # 40%
+python aps.py -n 50 -l 50 --iso -c 100 -s 600 > results/iso_50.txt & # 50%
 
 # anisotropic
 python aps.py -n 90 -l 10 -c 100 -s 600  > results/aniso_10.txt & # 10%
